562b9e4645d1c053a4c46ee8e83543127997d012
Ahmed__Ehab <ahmedehab11@gmail.com>
2020-02-03 19:49:45 +0200

    Keypad Update

--- Keypad.h
+++ Keypad.h
@@ -71,4 +71,29 @@ uint8 Keypad_Get_Data_from_Buffer (uint8* const );
 void Keypad_Clear_Buffer(void);
 
 
+//-------------------------------------------------------- Section of Keypad without OS and it can use Timeout mechanism ---------------------------------------------------------------//
+
+// Determine your Keypad Dimension
+#define N_COL  3
+#define N_ROW  3
+
+// Determine your Digit Size for your input
+#define DIGIT_SIZE 3
+
+#define SIZE   6
+#define YES	   1
+#define NO     0
+
+#define TIMEROUT_VALUE 18
+
+// Determine whether you need to return Integer Numbers from keypad or Ascii Value (Characters) from KeyPad
+#define PRINT_NUMBERS_From_KEYPAD YES
+
+
+//----------------------- Section of Public Function Prototype of Keypad API's -----------------//
+short  KEYPAD_INPUT_Number(void);
+char *  KEYPAD_INPUT_String(void);
+
+
+
 #endif // __Keypad_H__
